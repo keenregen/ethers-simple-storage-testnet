442831e2cdfa119bf0d09850ecb4c0f21474c0f0
Keen Regen <keenregen@gmail.com>
2023-02-01 01:54:06 +0300

    A few changes

--- deploy.js
+++ deploy.js
@@ -11,23 +11,29 @@ const main = async () => {
 
   const wallet = new ethers.Wallet(process.env.privateKey, provider);
 
-  const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi", "utf-8");
 
-  const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin", "utf8");
+  const binary = fs.readFileSync(
+    "SimpleStorage_sol_SimpleStorage.bin",
+    "utf-8"
+  );
 
   // deploying a contract
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying, be patient.");
   const contract = await contractFactory.deploy();
-  const receipt = await contract.deployTransaction.wait();
+  await contract.deployTransaction.wait("1");
+  console.log(`Contract Address: ${contract.address}`);
 
   // interacting with contract
   // retrieve the current fav num
   let currentFavNum = await contract.retrieve();
   console.log(`Current Fav Number: ${currentFavNum.toString()}`);
   // store a new fav num
-  const storeResp = await contract.store("41");
-  const receipt2 = await contract.deployTransaction.wait();
+  const storeResp = await contract.store("11111");
+  const receipt2 = await storeResp.wait("1");
+  // console.log(receipt2);
+
   currentFavNum = await contract.retrieve();
   console.log(`The Fav Number now: ${currentFavNum.toString()}`);
 };
